8c004c48dc980eaa593ca820ed9cd9071973d799
Paweł Nowak <apach87@gmail.com>
2018-07-08 19:52:29 +0200

    =v0.2.1

--- Twig/GoogleMapTwig.php
+++ Twig/GoogleMapTwig.php
@@ -28,6 +28,8 @@ class GoogleMapTwig extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFilter('gmap', array($this, 'staticMapFilter')),
+            new \Twig_SimpleFilter('staticGmap', array($this, 'staticMapFilter')),
+            new \Twig_SimpleFilter('markerGmap', array($this, 'markerMapFilter')),
         );
     }
 
@@ -43,6 +45,29 @@ class GoogleMapTwig extends \Twig_Extension
             'ApaczMapBundle:GoogleMap:template.html.twig',
             array('zoom' => $this->zoom, 'key' => $this->googleApiKey, 'coordinates' => $coordinates,
                 'width' => $width, 'height' => $height)
+        );
+
+        return $content;
+    }
+
+    /**
+     * @param iGeographicalCoordinates $coordinates
+     * @param int $width
+     * @param int $height
+     * @return mixed|string
+     */
+    public function markerMapFilter(iGeographicalCoordinates $coordinates, $width = 100, $height = 100, $id = 'map')
+    {
+        if(is_numeric($width)){
+            $width = $width.'px';
+        }
+        if(is_numeric($height)){
+            $height = $height.'px';
+        }
+        $content = $this->twig->render(
+            'ApaczMapBundle:GoogleMap:template-marker.html.twig',
+            array('zoom' => $this->zoom, 'key' => $this->googleApiKey, 'coordinates' => $coordinates,
+                'width' => $width, 'height' => $height, 'id' => $id)
         );
 
         return $content;
